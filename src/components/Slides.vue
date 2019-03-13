529930272866b9eed45e0376d9950703a75733e0
Imogen Hallett <imogen.hallett@accenture.com>
2019-03-13 21:14:20 +0000

    slides html added

--- src/components/Slides.vue
+++ src/components/Slides.vue
@@ -1,6 +1,47 @@
 <template>
-  <div>
-    <h1>Slides</h1>
+  <div class="container-fluid">
+    <div id="slides-wrap" class="row">
+      <div id="slide-1"
+        class="d-flex flex-row flex-wrap justify-content-center align-content-center
+          slide animated"
+      >
+        <div class="content">
+          <h2 class="text-center">Title Slide 1</h2>
+          <p class="text-center">Some text</p>
+          <p class="text-center">Some text</p>
+        </div>
+      </div>
+      <div id="slide-2"
+        class="d-flex flex-row flex-wrap justify-content-center align-content-center
+          slide slide hide-slide animated"
+      >
+        <div class="content">
+          <h2 class="text-center">Title Slide 2</h2>
+          <p class="text-center">Some text</p>
+          <p class="text-center">Some text</p>
+        </div>
+      </div>
+      <div id="slide-3"
+        class="d-flex flex-row flex-wrap justify-content-center align-content-center
+          slide hide-slide animated"
+      >
+        <div class="content">
+          <h2 class="text-center">Title Slide 3</h2>
+          <p class="text-center">Some text</p>
+          <p class="text-center">Some text</p>
+        </div>
+      </div>
+      <div id="slide-4"
+        class="d-flex flex-row flex-wrap justify-content-center align-content-center
+          slide hide-slide animated"
+      >
+        <div class="content">
+          <h2 class="text-center">Title Slide 4</h2>
+          <p class="text-center">Some text</p>
+          <p class="text-center">Some text</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
