f4cc2e36d6c70b938bf8a434933b301ef6807477
Imogen Hallett <imogen.hallett@accenture.com>
2019-03-13 21:07:19 +0000

    with nav example setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <app-slides></app-slides>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Slides from '../components/Slides.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    'app-slides': Slides,
   },
 };
 </script>
