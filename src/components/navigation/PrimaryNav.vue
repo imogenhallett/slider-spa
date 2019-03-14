5bd0ea378d89d5e226b90a858eb6040f138a7e36
Imogen Hallett <imogen.hallett@accenture.com>
2019-03-14 23:01:41 +0000

    added active class to primary nav

--- src/components/navigation/PrimaryNav.vue
+++ src/components/navigation/PrimaryNav.vue
@@ -13,19 +13,34 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav mx-auto">
-          <li class="nav-item my-3 my-lg-0 mx-3" @click="jumpToSlide(1)">
+          <li class="nav-item my-3 my-lg-0 mx-3"
+            @click="jumpToSlide(1)"
+            v-bind:class="{'active':(1 === getCurrentSlide)}"
+          >
             Customer Problem
           </li>
-          <li class="nav-item my-3 my-lg-0 mx-3" @click="jumpToSlide(2)">
+          <li class="nav-item my-3 my-lg-0 mx-3"
+            @click="jumpToSlide(2)"
+            v-bind:class="{'active':(2 === getCurrentSlide)}"
+          >
             Business Problem
           </li>
-          <li class="nav-item my-3 my-lg-0 mx-3" @click="jumpToSlide(3)">
+          <li class="nav-item my-3 my-lg-0 mx-3"
+            @click="jumpToSlide(3)"
+            v-bind:class="{'active':(3 === getCurrentSlide)}"
+          >
             Recommendations
           </li>
-          <li class="nav-item my-3 my-lg-0 mx-3" @click="jumpToSlide(4)">
+          <li class="nav-item my-3 my-lg-0 mx-3"
+            @click="jumpToSlide(4)"
+            v-bind:class="{'active':(4 === getCurrentSlide)}"
+          >
             Data Strategy
           </li>
-          <li class="nav-item my-3 my-lg-0 mx-3" @click="jumpToSlide(5)">
+          <li class="nav-item my-3 my-lg-0 mx-3"
+            @click="jumpToSlide(5)"
+            v-bind:class="{'active':(5 === getCurrentSlide)}"
+          >
             Next Steps
           </li>
         </ul>
